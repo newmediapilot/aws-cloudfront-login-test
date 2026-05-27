eba147a51cb07f9c8ab6388c10b18db2ee017de9
newmediapilot <marcin@newmediapilot.com>
2026-05-26 22:29:45 -0400

    Update auth.js

--- functions/auth.js
+++ functions/auth.js
@@ -1,14 +1,13 @@
 function handler(event) {
     var request = event.request;
     var cookies = request.cookies;
-
     var auth = cookies.auth && cookies.auth.value;
 
     if (auth === "valid-user") {
         request.headers["x-auth-status"] = { value: "allowed" };
         return request;
     }
-
+    
     return {
         statusCode: 302,
         statusDescription: "Found",
